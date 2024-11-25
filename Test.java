3bef840393ea5098c6215f245742da3662c2a21c
Anusara97 <anusara777rcm@gmail.com>
2024-11-25 14:07:55 +0530

    Tested upto now (2).

--- Test.java
+++ Test.java
@@ -3,7 +3,11 @@ public class Test {
         Contacts c1 = new Contacts("saman@sjp.ac.lk", "071-7206401", "No 2, Colombo 07");
         Author a1 = new Author("Saman Perera", c1);
 
+        Contacts c2 = new Contacts("dean@sjp.ac.lk", "011-2500400", "Nugegoda");
+        Publisher p1 = new Publisher("SJP", c2);
+
         // System.out.println(c1);
-        System.out.println(a1);
+        // System.out.println(a1);
+        System.out.println(p1);
     }
 }
