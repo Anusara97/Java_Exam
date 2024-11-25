dc8f59df2bb068daaa363fc3441295f2d95354a9
Anusara97 <anusara777rcm@gmail.com>
2024-11-25 13:22:08 +0530

    Test.java created and tested upto now.

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        Contacts c1 = new Contacts("anusara@gmail.com", "0712223334444", "Matara");
+
+        System.out.println(c1);
+    }
+}
