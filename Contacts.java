170a3ee0cb523791b1c9e1cc781564520300dd30
Anusara97 <anusara777rcm@gmail.com>
2024-11-25 11:48:00 +0530

    Contacts.java created.

---
+++ Contacts.java
@@ -0,0 +1,43 @@
+public class Contacts {
+    private String email;
+    private String telephone;
+    private String address;
+
+    public Contacts(String email) {
+        this(email, "not given");
+    }
+
+    public Contacts(String email, String telephone) {
+        this(email, telephone, "not given");
+    }
+
+    public Contacts(String email, String telephone, String address) {
+        setEmail(email);
+        setTelephone(telephone);
+        setAddress(address);
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
