60af0aec3895399b4abd0c1a8b1f6405768761a3
Anusara97 <anusara777rcm@gmail.com>
2024-11-25 13:55:17 +0530

    Tested upto now.

--- Test.java
+++ Test.java
@@ -1,7 +1,9 @@
 public class Test {
     public static void main(String[] args) {
-        Contacts c1 = new Contacts("anusara@gmail.com", "0712223334444", "Matara");
+        Contacts c1 = new Contacts("saman@sjp.ac.lk", "071-7206401", "No 2, Colombo 07");
+        Author a1 = new Author("Saman Perera", c1);
 
-        System.out.println(c1);
+        // System.out.println(c1);
+        System.out.println(a1);
     }
 }
